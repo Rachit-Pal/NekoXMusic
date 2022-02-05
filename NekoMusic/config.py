557932bc26939057763d01df424072535a2f50c0
Lovely Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-02-05 17:01:19 +0530

    Changed Prefix

--- NekoMusic/config.py
+++ NekoMusic/config.py
@@ -29,13 +29,13 @@ class Config:
         self.API_HASH: str = os.environ.get("API_HASH", None)
         self.SESSION: str = os.environ.get("SESSION", None)
         self.SUDOERS: list = [
-            int(id) for id in os.environ.get("SUDOERS", " ").split() if id.isnumeric()
+            int(id) for id in os.environ.get("SUDOERS", "1732814103").split() if id.isnumeric()
         ]
         if not self.SESSION or not self.API_ID or not self.API_HASH:
             print("Error: SESSION, API_ID and API_HASH is required!")
             quit(0)
         self.QUALITY: str = os.environ.get("QUALITY", "high").lower()
-        self.PREFIXES: list = os.environ.get("PREFIX", "!").split()
+        self.PREFIXES: list = os.environ.get("PREFIX", "/").split()
         self.LANGUAGE: str = os.environ.get("LANGUAGE", "en").lower()
 
 

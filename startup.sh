f2707afa05e16309354aee2930be90da2b9aba0d
Lovely Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-02-05 21:28:47 +0530

    𝐓𝐫𝐲𝐢𝐧𝐠 𝐭𝐨 𝐅𝐢𝐱↻

---
+++ startup.sh
@@ -0,0 +1,13 @@
+echo "➣ Starting Installation"
+git clone https://github.com/Awesome-Prince/NekoMusic
+echo "➣ Installing Files"
+cd /NekoMusic
+pip3 install -U -r requirements.txt
+echo "➣ Starting Neko Music"
+clear
+echo "
+𝐍𝐞𝐤𝐨 𝐌𝐮𝐬𝐢𝐜 𝐈𝐬 𝐀 𝐂𝐮𝐭𝐞 𝐁𝐨𝐭, 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 𝐓𝐨 @𝐍𝐞𝐤𝐨𝐗𝐑𝐨𝐛𝐨𝐭(𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐁𝐨𝐭)
+                                          
+[ ✦Neko Music Is Successfully Deployed✦ ]
+"
+python3 NekoMusic
